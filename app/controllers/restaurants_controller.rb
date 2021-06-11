c0d1e8b8934206acb3666fc55eaf81c7a64a7be9
Nico Proto <nicolasproto@gmail.com>
2021-06-11 10:21:46 +0200

    Added stimulus and finished lecture

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { restaurants: @restaurants } }
+    end
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     # Is for the "review#new form"
     @review = Review.new
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 end
